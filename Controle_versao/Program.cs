01fb55e4806c381ce533440aff476d75d1dee630
Fabio Lazari Costa <flc.rock13@gmail.com>
2020-05-16 11:06:28 -0300

    Projeto iniciado, mostrando mensagem Ola mundo

---
+++ Controle_versao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle_versao
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Olá Mundo!");
+
+			Console.ReadKey();
+		}
+	}
+}
